8879314ab22624a42b6fa4a843b80264c649ff62
Gyssais <julien.chatelais@free.fr>
2016-02-14 16:59:36 +0100

    Affiche un truc inutile

--- open_image.cpp
+++ open_image.cpp
@@ -26,6 +26,7 @@ int main( int argc, char** argv )
     imshow( "Display window", image );                   // Show our image inside it.
 
     cout << "Affichage de : " << argv[1] << endl;
+    cout << "Ceci est une modif faite sur la branche newbranch" << endl;
     waitKey(0);                                          // Wait for a keystroke in the window
     return 0;
 }
