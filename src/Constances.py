0a3614c1176fd084e95965d8c87946958c108dcc
Vu Hoang PHAN <mrfantasiospirou@gmail.com>
2016-03-02 18:33:21 +0100

    La reconnaissance de couleurs marche

--- src/Constances.py
+++ src/Constances.py
@@ -1,3 +1,5 @@
+# All global constants go here
+
 MAX_PIXEL_VAL = 255
 
 ALL_CONTOURS = -1
@@ -6,7 +8,10 @@ H_MAX = 179
 S_MAX = 255
 V_MAX = 255
 
-TOLERANCE = 0.30
+TOLERANCE = 0.15
+
+GREEN = (0, 255, 0)
+CONTOURS_THICKNESS = 5
 
 
 
--- src/Track_Color_Objects.py
+++ src/Track_Color_Objects.py
@@ -1,68 +1,79 @@
 import cv2
 import numpy as np
-from Morph_Trans import *
 from Contours import *
 from Constances import *
 
 
-m_x = 0
-m_y = 0
-hsv = [0, 0, 0]
-lower_hsv = np.array([0, 0, 0])
-upper_hsv = np.array([0, 0, 0])
+pixel_color = [0, 0, 0]
+lower_pixel_color = np.array([0, 0, 0])
+upper_pixel_color = np.array([0, 0, 0])
 
 
+# Return a pixel information at a point (x,y)
+def get_pixel_color_point(filtered_frame, x, y):
+	return filtered_frame[y, x]
+
+
+# Return two threshold values acceptable for a color within a tolerance of TOLERANCE
+def set_pixel_color_thresh(pixel_color):
+	global upper_pixel_color, lower_pixel_color
+	# Set lower and upper thresholds
+	upper_pixel_color = np.array([pixel_color[0]*(1+TOLERANCE), pixel_color[1]*(1+TOLERANCE), pixel_color[2]*(1+TOLERANCE)])
+	lower_pixel_color = np.array([pixel_color[0]*(1-TOLERANCE), pixel_color[1]*(1-TOLERANCE), pixel_color[2]*(1-TOLERANCE)])
+	return upper_pixel_color, lower_pixel_color
+
+
+# Set mouse left click event, each left click will return the mouse's position (x,y) and the pixel's three color values
 def on_mouse(event, x, y, flag, param):
-	global m_x, m_y, hsv_frame, hsv, upper_hsv, lower_hsv
+	global filtered_frame, pixel_color, upper_pixel_color, lower_pixel_color
 	if event == cv2.EVENT_LBUTTONDOWN:
-		m_x = x
-		m_y = y
-		hsv = get_hsv_point(hsv_frame, x, y)
-		upper_hsv, lower_hsv = set_hsv_thresh(hsv)
+		# Get mouse position and update color thresholds
+		pixel_color = get_pixel_color_point(filtered_frame, x, y)
+		upper_pixel_color, lower_pixel_color = set_pixel_color_thresh(pixel_color)
+		# Print on-click information
+		print(x, y, pixel_color[0], pixel_color[1], pixel_color[2])
 	return None
 
 
-def get_hsv_point(hsv_frame, x, y):
-	return hsv_frame[x, y]
+if __name__ == '__main__':
 
+	# Create windows for actual image, filtered image and masked image
+	cv2.namedWindow("cam")
+	cv2.namedWindow("filtered")
+	cv2.namedWindow("mask")
 
-def set_hsv_thresh(hsv):
-	upper_hsv = np.array([hsv[0]*(1+TOLERANCE), hsv[1]*(1+TOLERANCE), hsv[2]*(1+TOLERANCE)])
-	lower_hsv = np.array([hsv[0]*(1-TOLERANCE), hsv[1]*(1-TOLERANCE), hsv[2]*(1-TOLERANCE)])
-	return upper_hsv, lower_hsv
+	# Set mouse handler event on the filtered window
+	cv2.setMouseCallback("filtered", on_mouse)
 
+	# Initialize the camera
+	cam = cv2.VideoCapture(0)
 
-cv2.namedWindow("cam")
-cv2.namedWindow("hsv")
-cv2.setMouseCallback("hsv", on_mouse)
-cam = cv2.VideoCapture(0)
+	# Loop forever
+	while cv2.waitKey(1) == -1:
 
-while cv2.waitKey(1) == -1:
+		# Read image from camera and get the next image
+		ret_cam, frame = cam.read()
 
-	ret_cam, frame = cam.read()
+		# Blur image in order to remove some noises
+		frame = cv2.GaussianBlur(frame, (11, 11), 5)
 
-	frame = cv2.GaussianBlur(frame, (11, 11), 5)
+		# Filter actual image
+		filtered_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	hsv_frame = frame
-	# hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
+		# Mask image, only keep the colors inside the thresholds
+		mask_frame = cv2.inRange(filtered_frame, lower_pixel_color, upper_pixel_color)
 
-	print(m_x, m_y, hsv[0], hsv[1], hsv[2])
+		# Simply just draw contours
+		draw_contours(frame, mask_frame, GREEN, CONTOURS_THICKNESS)
 
-	mask_frame = cv2.inRange(hsv_frame, lower_hsv, upper_hsv)
+		# Show all three windows
+		cv2.imshow("filtered", filtered_frame)
+		cv2.imshow("cam", frame)
+		cv2.imshow("mask", mask_frame)
 
-	# mask_frame = opening_op(mask_frame)
-	# mask_frame = cv2.erode(mask_frame, K_SIZE, iterations=2)
-	# mask_frame = cv2.dilate(mask_frame, K_SIZE, iterations=2)
-
-	# draw_contours(frame, mask_frame, CONTOURS_COLOR)
-
-	cv2.imshow("hsv", hsv_frame)
-	cv2.imshow("cam", frame)
-	cv2.imshow("mask", mask_frame)
-
-
-cv2.destroyAllWindows()
-cam.release()
+	# Escape
+	cv2.destroyAllWindows()
+	cam.release()
 
 
 
