0e5cc2e52337a5eac223aed8f7c526cf1ba0db12
Gyssais <julien.chatelais@free.fr>
2016-05-16 12:24:38 +0200

    Mes changements

--- Traitement_image/Detection_PC/comPi.sh
+++ Traitement_image/Detection_PC/comPi.sh
@@ -3,7 +3,8 @@
 
 while true
 do
-    sshpass -p 'raspberry' scp pi@192.168.43.94:~/Projet_drone/src/report.txt ./
+    sshpass -p 'raspberry' scp pi@192.168.43.94:~/Projet_drone/Traitement_image/Detection_Pi/report.txt ./
     cat report.txt
+    sshpass -p 'raspberry' scp pi@192.168.43.94:~/Projet_drone/Traitement_image/Detection_Pi/img.jpeg ./
     sleep 1
 done
