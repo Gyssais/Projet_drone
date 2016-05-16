9b70b052c217bb15fa2df8ae05358c5c00a05704
The00 <leurent@etud.insa-toulouse.fr>
2016-05-16 10:58:43 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 from src.Traitement_image.Detection_Pi.picam import *
-
+import dcapi
 
 if __name__ == '__main__':
 
     cam = PiCam()
     cam.initialize()
-
+    dcapi.init()
+    
     Thread(target=cam.run, name='detector').start()
-    Thread(target=gpio.run, name='gpio').start()
+    Thread(target=dcapi.run, name='autopilot').start()
 
 
 
